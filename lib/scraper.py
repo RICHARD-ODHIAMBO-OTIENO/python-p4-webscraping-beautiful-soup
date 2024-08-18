94bd458dc016039f1b5fbb6c9560192c955b6116
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-08-18 15:13:34 +0300

    Python Scraper Commit

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,13 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+# Save the HTML document
+doc = BeautifulSoup(html.text, 'html.parser')
+
+# Select the elements with the specified classes
+courses = doc.select('.heading-60-black.color-black.mb-20')
+
+# Print the content of each course element
+for course in courses: 
+  print(course.contents[0].strip())
